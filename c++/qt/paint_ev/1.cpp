f9740602be169ae43fdc87120d144b3b9bc5b00b
Hrompic <semenxx@gmail.com>
2019-12-28 21:55:49 +0200

    some changes

--- c++/qt/paint_ev/1.cpp
+++ c++/qt/paint_ev/1.cpp
@@ -11,32 +11,19 @@
 
 using namespace std;
 using namespace Qt;
-/*
-class ball
-{
-private:
-	int x;
-	int y;
-public:
-	ball(int _x, int _y):x(_x),y(_y)
-	{
-	
-	}
-	
-};
-*/
+
 class myw: public QWidget
 {
 public:
-	myw(QWidget* p=0);
-	~myw(){}
+    myw(QWidget* p=nullptr);
 	void paintEvent(QPaintEvent*) override;
 	void resizeEvent(QResizeEvent*) override;
-	void keyPressEvent(QKeyEvent *event) override;
+    void keyPressEvent(QKeyEvent*) override;
 private:
 	QTimer *time;
 	QRect ball;
 };
+
 int main(int argc, char** argv)
 {
 	QApplication a(argc, argv);
@@ -47,7 +34,8 @@ int main(int argc, char** argv)
 	w.show();
 	return a.exec();
 }
-void myw::paintEvent(QPaintEvent* event)
+
+void myw::paintEvent(QPaintEvent*)
 {
 	QPainter paint(this);
 	//ball = QRect(width()/2, height()/2, width()/4, height()/4);
@@ -56,21 +44,29 @@ void myw::paintEvent(QPaintEvent* event)
 	paint.setBrush(QBrush(red, DiagCrossPattern));
 	paint.drawEllipse(ball);
 }
+
 void myw::resizeEvent(QResizeEvent*)
 {
 	ball.setSize(size()/8);
 }
+
 void myw::keyPressEvent(QKeyEvent *event)
 {
+
 	static int h = 0;
-	qDebug() <<event <<ball;
-	if((Key_Left==event->key()) ){ ball.moveLeft(--h); update(); return;}
-	//else if((Key_Right==event->key()) && (ball.x()<width())){ ball.moveLeft(1); update(); return;}
-	else if((Key_Right==event->key()) ){ ball.moveLeft(h); update(); h++; return;}
+    static int w = 0;
+    int speed = 10;
+    qDebug() <<event <<ball <<h <<ball.bottom();
+    if((Key_Up==event->key()) && (ball.top()>0)) {ball.moveTop(--w*speed); update(); return;}
+    else if((Key_Down==event->key()) && (ball.bottom()<this->size().height()-10)) {ball.moveTop(++w*speed); update(); return;}
+    else if((Key_Left==event->key()) && (ball.left()>0)) {ball.moveLeft(--h*speed); update(); return;}
+    else if((Key_Right==event->key()) && (ball.right()<this->size().width()-1)) {ball.moveLeft(++h*speed); update(); return;}
 }
-myw::myw(QWidget* p):QWidget(p)
+
+myw::myw(QWidget* p)
+    :QWidget(p)
 {
 	time = new QTimer(this);
 	time->start();
-	ball = QRect(QPoint(), size()/8);
+    ball = QRect(QPoint(), (size()/8));
 }
