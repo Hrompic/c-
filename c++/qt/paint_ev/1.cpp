6e38c98b77d36ceda59f062b45d8767bb4329e0d
Hrompic <semenxx@gmail.com>
2020-01-02 19:38:34 +0200

    update

--- c++/qt/paint_ev/1.cpp
+++ c++/qt/paint_ev/1.cpp
@@ -1,4 +1,4 @@
-#include <QApplication>
+﻿#include <QApplication>
 #include <QPainter>
 #include <QRect>
 #include <QWidget>
@@ -16,57 +16,81 @@ class myw: public QWidget
 {
 public:
     myw(QWidget* p=nullptr);
-	void paintEvent(QPaintEvent*) override;
-	void resizeEvent(QResizeEvent*) override;
+    void paintEvent(QPaintEvent*) override;
+    void resizeEvent(QResizeEvent*) override;
     void keyPressEvent(QKeyEvent*) override;
 private:
-	QTimer *time;
-	QRect ball;
+    //QTimer *time;
+    QRect ball;
+    enum moves{up, down, left, right};
+    void move(moves);
+private slots:
+    void drawtext();
 };
 
 int main(int argc, char** argv)
 {
-	QApplication a(argc, argv);
-	myw w;
-	
-	
-	w.resize(800, 600);
-	w.show();
-	return a.exec();
+    QApplication a(argc, argv);
+    myw w;
+
+
+    w.resize(800, 600);
+    w.show();
+    return a.exec();
 }
 
 void myw::paintEvent(QPaintEvent*)
 {
-	QPainter paint(this);
-	//ball = QRect(width()/2, height()/2, width()/4, height()/4);
-
-	paint.setPen(QPen(black, 5, SolidLine)); 
-	paint.setBrush(QBrush(red, DiagCrossPattern));
-	paint.drawEllipse(ball);
+    QPainter paint(this);
+    paint.setBrush(QBrush(blue, Dense2Pattern));
+    paint.drawRect(rect());
+#if 1
+    paint.setBrush(QBrush(white, SolidPattern));
+    paint.drawEllipse(ball);
+#endif
+    paint.setPen(QPen(green, 3, SolidLine));
+    paint.setBrush(QBrush(red, Dense1Pattern));
+    paint.drawEllipse(ball);
 }
 
 void myw::resizeEvent(QResizeEvent*)
 {
-	ball.setSize(size()/8);
+    ball.setSize(size()/8);
 }
 
 void myw::keyPressEvent(QKeyEvent *event)
 {
+    qDebug() <<event <<ball;
+    if((Key_Up==event->key()) && (ball.top()>0)) {move(up); return;}
+    else if((Key_Down==event->key()) && (ball.bottom()<this->size().height()-10)) {move(down); return;}
+    else if((Key_Left==event->key()) && (ball.left()>0)) {move(left); return;}
+    else if((Key_Right==event->key()) && (ball.right()<this->size().width()-1)) {move(right); return;}
+}
 
-	static int h = 0;
+void myw::move(moves mv)
+{
+    static int h = 0;
     static int w = 0;
     int speed = 10;
-    qDebug() <<event <<ball <<h <<ball.bottom();
-    if((Key_Up==event->key()) && (ball.top()>0)) {ball.moveTop(--w*speed); update(); return;}
-    else if((Key_Down==event->key()) && (ball.bottom()<this->size().height()-10)) {ball.moveTop(++w*speed); update(); return;}
-    else if((Key_Left==event->key()) && (ball.left()>0)) {ball.moveLeft(--h*speed); update(); return;}
-    else if((Key_Right==event->key()) && (ball.right()<this->size().width()-1)) {ball.moveLeft(++h*speed); update(); return;}
+    switch (mv)
+    {
+        case up: {ball.moveTop(--w*speed); update();return;}
+        case down: {ball.moveTop(++w*speed); update(); return;}
+        case left:  {ball.moveLeft(--h*speed); update(); return;}
+        case right: {ball.moveLeft(++h*speed); update(); return;}
+    }
+}
+
+void myw::drawtext()
+{
+
 }
 
 myw::myw(QWidget* p)
     :QWidget(p)
 {
-	time = new QTimer(this);
-	time->start();
+//    time = new QTimer(this);
+//    time->start(50000);
+//    QObject::connect(time, SIGNAL(timeout()), this, SLOT(close()));
     ball = QRect(QPoint(), (size()/8));
 }
